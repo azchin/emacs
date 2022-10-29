a0aa102d4d43f59c514e1f2f1c74b8f0200b8f30
Andrew Chin <correctbutnotright@outlook.com>
2022-10-29 14:01:31 -0400

    Missed files in previous commit for gnus and evil

---
+++ core/gnus.el
@@ -0,0 +1,6 @@
+(setq gnus-select-method '(nnnil))
+(setq gnus-secondary-select-methods
+      '((nntp "news.gwene.org")))
+
+(add-to-list 'newsticker-url-list
+             '("CISA Current Activity" "https://www.cisa.gov/uscert/ncas/current-activity.xml"))
